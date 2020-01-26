adc75d2880b5f39647f7ec4fbe66fc8c63f433a3
Zaim Bakar <37629946+zzzaim@users.noreply.github.com>
2020-01-26 20:16:20 +0800

    Add contact page stub

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+
+export default () => (
+  <React.Fragment>
+    <Helmet>
+      <title>Contact</title>
+    </Helmet>
+  </React.Fragment>
+)
